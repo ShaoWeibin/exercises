10800862b27077094ca33892f8849e10637c01b3
weibin.shao <weibin.shao@alibaba-inc.com>
2016-06-19 23:12:56 +0800

    exercise02 删除重复html代码 使用js动态创建

--- exercise02/index.js
+++ exercise02/index.js
@@ -4,10 +4,7 @@
  */
 
 (function appendArticles() {
-    alert('hello');
-
-    var likes = [];
-    var answers = [];
+    var dataset = [];
 
     var like = new Like();
     like.likes = 128;
@@ -16,40 +13,182 @@
     like.date = '3 天前 15:21';
     like.title = '罗振宇、罗永浩、阿禅粉丝那么多，创业反不太成功？';
 
-    likes.push(like);
-    likes.push(like);
-    likes.push(like);
-    likes.push(like);
-    likes.push(like);
+    var answer = new Answer();
+    answer.likes = 528;
+    answer.views = '14.8K';
+    answer.name = '不贰';
+    answer.date = '3 天前 15:21';
+    answer.title = '视觉、交互、界面设计之间的关系是什么？';
+    answer.tags = ['Alex', 'Masslow', '鹿慕小姐', '芜昂昂', '坦率的爆米花', '秋日高照', 'Quinn Norton', '+528'];
 
-    appendLike(likes);
+    dataset.push(like);
+    dataset.push(answer);
+    dataset.push(like);
+    dataset.push(answer);
+    dataset.push(like);
+    dataset.push(answer);
+    dataset.push(like);
+    // dataset.push(answer);
+    // dataset.push(like);
+    // dataset.push(answer);
+    // dataset.push(like);
+    // dataset.push(answer);
+
+    dataset.forEach(function (d, i) {
+        var set = [];
+
+        var n = (i % 2 == 1) ? i + 1 : i + 2;
+        for (var j = 0; j < i + 1; ++j)
+            set.push(d);
+
+        if (d instanceof Like)
+            appendLike(set);
+        else if (d instanceof Answer)
+            appendAnswer(set);
+        else ;  // do nothing;
+    });
+
+    // 更新推荐关注头像
+    attentionlist();
 })();
 
 function appendLike(likes) {
-    var list = d3.select(".main-discovery-list");
-    var like = list.selectAll('.main-discovery-article-like');
-    // likes.forEach(function(item, index, array){
-    //     list.append('.main-discovery-article-like');
-    // });
+    var $list = d3.select(".main-discovery-list");
+    var $items = $list.selectAll('.main-discovery-article-like').data(likes);
 
-    for(var item in likes){
-        //list.append('.main-discovery-article-like');
-    }
+    var $enter = $items.enter().append('div')
+        .attr('class', 'main-discovery-article-like');
 
-    // like.data(likes)
-    //     .enter()
-    //     .append('.main-discovery-article-like');
-    //
-    // list.data(likes)
-    //     .enter()
-    //     .append('h1')
-    //     .text(function (d) {
-    //         return "test" + d.name;
-    //     });
+    var $enter1 = $enter.append('div').attr('class', 'article-like');
+
+    $enter1.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .text(function(d){
+            return d.likes;
+        });
+
+    $enter1.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .append('small')
+        .text(function(d){
+            return 'likes';
+        });
+
+    var $enter2 = $enter.append('div').attr('class', 'article-views gray-font');
+
+    $enter2.append('div')
+        .append('span')
+        .text(function(d){
+            return d.views;
+        });
+
+    $enter2.append('div')
+        .append('span')
+        .append('small')
+        .text(function(d){
+            return 'views';
+        });
+
+    var $enter3 = $enter.append('div').attr('class', 'article-title');
+
+    $enter3.append('div')
+        .append('span')
+        .attr('class', 'gray-font')
+        .append('small')
+        .text(function(d){
+            return d.name + ' ' + d.date;
+        });
+
+    $enter3.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .text(function(d){
+            return d.title;
+        });
 }
 
 function appendAnswer(answers) {
-    
+    var $list = d3.select(".main-discovery-list");
+    var $items = $list.selectAll('.main-discovery-article-answers').data(answers);
+
+    var $enter = $items.enter().append('div')
+        .attr('class', 'main-discovery-article-answers');
+
+    var $enter1 = $enter.append('div').attr('class', 'article-answers');
+
+    $enter1.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .text(function(d){
+            return d.likes;
+        });
+
+    $enter1.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .append('small')
+        .text(function(d){
+            return 'likes';
+        });
+
+    var $enter2 = $enter.append('div').attr('class', 'article-views gray-font');
+
+    $enter2.append('div')
+        .append('span')
+        .text(function(d){
+            return d.views;
+        });
+
+    $enter2.append('div')
+        .append('span')
+        .append('small')
+        .text(function(d){
+            return 'views';
+        });
+
+    var $enter3 = $enter.append('div').attr('class', 'article-title');
+
+    $enter3.append('div')
+        .append('span')
+        .attr('class', 'gray-font')
+        .append('small')
+        .text(function(d){
+            return d.name + ' ' + d.date;
+        });
+
+    $enter3.append('div')
+        .append('span')
+        .attr('class', 'bolder')
+        .text(function(d){
+            return d.title;
+        });
+
+    var $enter31 = $enter3.append('div')
+        .append('span')
+        .attr('class', 'gray-font');
+
+    $items = $enter31.selectAll('small')
+        .data(function (d) {
+            return d.tags;
+        });
+
+    $items.enter()
+        .append('small')
+        .text(function (d) {
+            return d;
+        });
+}
+
+function attentionlist(){
+    var dataset = ['personal-img-mini.jpg', 'personal-img-mini-2.jpg', 'personal-img-mini-3.jpg'];
+
+    var list = d3.select(".attention-list").selectAll('.attention-info >div:first-child');
+    list.data(dataset)
+        .style('background', function (d) {
+            return 'url(img/' + d + ')';
+        });
 }
 
 function Like() {
